096b00e16fdfab1be5be96e183b006460d38f354
Manish <manishpaudel290@gmail.com>
2024-05-18 12:49:14 +0545

    react app all files added to git

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Profile } from './react_files/profile';
+import Gallery from './react_files/gallery';
+
+function App() {
+  return (
+   <div>
+    <Profile/>
+    <Gallery/>
+   </div>
+    
+  );
+}
+
+export default App;
---
+++ react-app/src/react_files/gallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Profile} from './profile';
+
+
+
+ const Gallery=()=>  {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+export default Gallery;
