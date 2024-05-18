c1007c3a3921c840da35c8e1633fbbe24ae04c94
Manish <manishpaudel290@gmail.com>
2024-05-18 16:12:21 +0545

    js curly braces updated

--- react-app/src/react_files/jscurly.js
+++ react-app/src/react_files/jscurly.js
@@ -1,7 +1,33 @@
-export  const JsCurly =()=>{
-    const name= 'Manish';
-    return(
-        <h1>{name}'s work list</h1>
-    )
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
 }
-export default JsCurly;
+
+export default function JsCurly() {
+  return (
+    <h1>To Do List for {formatDate(today)}</h1>
+  );
+}
+
+
+
+//   const today = Date();
+  
+// function TodayDate(date){
+//     return new Intl.DateTimeFormat(
+//         'en-US',
+//         {weekend:'long'}
+//     ).format(date);
+// }
+
+//   const JsCurly =()=>{
+//     const name= 'Manish';
+//     return(
+//         <h1>{TodayDate(today)} is the date</h1>
+//     )
+// }
+// export default JsCurly;
