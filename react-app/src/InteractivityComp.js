921b877febf430c583ff90b4c84fee0bdda00d36
Manish <manishpaudel290@gmail.com>
2024-05-21 13:33:06 +0545

    main activity file for components

---
+++ react-app/src/InteractivityComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClickButton from './interactivity/buttonClick';
+
+const InteractivityComponents = () => {
+  return (
+    <div>
+        <ClickButton/>
+    </div>
+  );
+}
+
+export default InteractivityComponents;
