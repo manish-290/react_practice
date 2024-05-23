86852f06a79c29cfd2d69b035228b62c0e45b54b
Manish <manishpaudel290@gmail.com>
2024-05-23 10:22:09 +0545

    initialize the use state in the top of the form  and set state

---
+++ react-app/src/interactivity/stuckForm.js
@@ -0,0 +1,48 @@
+// When you type into the input fields, nothing appears.
+//  It’s like the input values are “stuck” with empty strings.
+//   The value of the first <input> is set to always match the
+//    firstName variable, and the value for the second <input> 
+//    is set to always match the lastName variable. This is correct.
+//     Both inputs have onChange event handlers, which 
+//     try to update the variables based on the latest user 
+//     input (e.target.value). However, the variables don’t
+//      seem to “remember” their values between re-renders.
+//  Fix this by using state variables instead.
+
+
+import {useState} from 'react';
+
+export default function Form() {
+  const [firstName,setFirstName]=useState("");
+  const [lastName,setLastName]=useState("");
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleReset() {
+   setFirstName("");
+    setLastName("");
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  );
+}
