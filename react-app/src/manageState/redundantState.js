5066e81263fa3ba55e1e45ad5683fe40f1761d00
Manish <manishpaudel290@gmail.com>
2024-06-01 22:13:01 +0545

    reduce the redundant state in the react application

---
+++ react-app/src/manageState/redundantState.js
@@ -0,0 +1,41 @@
+//redudant state variable fullname
+//here we remove the redundant state fullname
+
+import { useState } from 'react';
+
+export default function RedundantForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const fullName = firstName + ' '+ lastName;
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Let’s check you in</h2>
+      <label>
+        First name:{' '}
+        <input
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:{' '}
+        <input
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+      <p>
+        Your ticket will be issued to: <b>{fullName}</b>
+      </p>
+    </>
+  );
+}
