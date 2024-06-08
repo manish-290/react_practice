ac021c3c387fb168e89b5f735c1d7a8e6d8a1fe7
Manish <manishpaudel290@gmail.com>
2024-06-08 22:37:56 +0545

    dom update

---
+++ react-app/src/manageState/updateDOM.js
@@ -0,0 +1,74 @@
+let firstName = 'Jane';
+let lastName = 'Jacobs';
+let isEditing = false;
+function handleFormSubmit(e) {
+  e.preventDefault();
+  setIsEditing(!isEditing);
+}
+
+function handleFirstNameChange(e) {
+  setFirstName(e.target.value);
+   helloText.textContent = "Hello "+ firstName+' '+ lastName;
+}
+
+function handleLastNameChange(e) {
+  setLastName(e.target.value);
+  helloText.textContent = "Hello "+ firstName +' '+ lastName;
+}
+
+function setFirstName(value) {
+  firstName = value;
+  updateDOM();
+}
+
+function setLastName(value) {
+  lastName = value;
+  updateDOM();
+}
+
+function setIsEditing(value) {
+  isEditing = value;
+  updateDOM();
+}
+
+function updateDOM() {
+  if (isEditing) {
+    editButton.textContent = 'Save Profile';
+    // TODO: show inputs, hide content
+    hide(firstNameText);
+    hide(lastNameText);
+    show(firstNameInput);
+    show(lastNameInput);
+    
+  } else {
+    editButton.textContent = 'Edit Profile';
+    // TODO: hide inputs, show content
+    show(firstNameText);
+    show(lastNameText);
+    hide(firstNameInput);
+    hide(lastNameInput);
+  }
+  // TODO: update text labels
+  firstNameText.textContent= firstName;
+  lastNameText.textContent= lastName;
+  
+}
+
+function hide(el) {
+  el.style.display = 'none';
+}
+
+function show(el) {
+  el.style.display = '';
+}
+
+let form = document.getElementById('form');
+let editButton = document.getElementById('editButton');
+let firstNameInput = document.getElementById('firstNameInput');
+let firstNameText = document.getElementById('firstNameText');
+let lastNameInput = document.getElementById('lastNameInput');
+let lastNameText = document.getElementById('lastNameText');
+let helloText = document.getElementById('helloText');
+form.onsubmit = handleFormSubmit;
+firstNameInput.oninput = handleFirstNameChange;
+lastNameInput.oninput = handleLastNameChange;
